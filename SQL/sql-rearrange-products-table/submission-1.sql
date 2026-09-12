595e768121c33546bfe8f086b5f9bcaf8bfb3ac8
Abhishekc18 <89436198+Abhishekc18@users.noreply.github.com>
2026-09-13 01:49:13 +0530

    Add: sql-rearrange-products-table - submission-1

---
+++ SQL/sql-rearrange-products-table/submission-1.sql
@@ -0,0 +1,5 @@
+select product_id, 'store1' as store, store1 as price from products where store1 is not null
+union all
+select Product_id, 'store2' as store2, store2 as price from products where store2 is not null
+union all
+select product_id, 'store3' as store, store3 as price from products where store3 is not null; 
