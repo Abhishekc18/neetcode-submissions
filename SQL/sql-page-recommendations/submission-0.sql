b3c165358a588bc939d791768f5d799bc518737b
Abhishekc18 <89436198+Abhishekc18@users.noreply.github.com>
2026-09-16 21:43:35 +0530

    Add: sql-page-recommendations - submission-0

---
+++ SQL/sql-page-recommendations/submission-0.sql
@@ -0,0 +1,10 @@
+-- Write your query below
+select distinct
+l.page_id as recommended_page 
+from (
+    select user2_id as friend_id from friendship where user1_id = 1
+    union
+    select user1_id as friend_id from friendship where user2_id = 1
+) f
+join likes l on f.friend_id = l.user_id
+where l.page_id not in (select page_id from likes where user_id = 1)
